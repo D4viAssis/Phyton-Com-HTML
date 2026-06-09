0225ae00a0e075df0f55e3d326c2b2e5a2ccaa8b
É o Davas <davi.assis.neves@escola.pr.gov.br>
2026-06-09 14:38:09 +0000

    Alteração 2

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-from flask import Flask, render_template app = Flask()@app.route('/') def inicio: return render_template('index.html') app.run(host = '0.0.0', port = 3000)
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    nome = "Turma de programação"
+    curso = "Phyton & HTML"
+
+    return render_template (
+        'index.html',
+        nome = nome,
+        curso = curso
+    )
+
+@app.route('/sobre')
+def sobre():
+    return """
+    <h1>Sobre a turma</h1>
+    <p>Esse projeto foi criado utilizando Python e HTML.</p>
+    <a href="/">Voltar para o inicio</a>
+    """
+
+    if __name__ == '__main __':
+        app.run(host='0.0.0.0.' port=3000, debug=true)
+    
+    
+
+    
